bc2133b716e8382f2e1c7ce3fa8bf8481e2f14c1
RemxFr <epourexisterpro@gmail.com>
2023-10-11 20:19:31 +0200

    Initial commit

---
+++ src/main/java/com/tpws/monthlyCalendar/MonthlyCalendarApplication.java
@@ -0,0 +1,17 @@
+package com.tpws.monthlyCalendar;
+
+import org.example.Calendar;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootApplication
+public class MonthlyCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpws/monthlyCalendar/MonthlyCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpws.monthlyCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
